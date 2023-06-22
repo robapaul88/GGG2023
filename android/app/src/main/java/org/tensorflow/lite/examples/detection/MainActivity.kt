2360e4dd9886251e0439139dfa3e3f49d5a9b4f3
Paul Roba <paul.roba@garmin.com>
2023-06-22 13:07:07 +0300

    Add crashlytics and a crash button

--- android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.kt
+++ android/app/src/main/java/org/tensorflow/lite/examples/detection/MainActivity.kt
@@ -14,11 +14,13 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import org.tensorflow.lite.examples.detection.ui.theme.AndroidTheme
+import java.lang.RuntimeException
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,17 +40,30 @@ fun ContentScreen() {
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
         Box(modifier = Modifier.fillMaxSize()) {
             Column(modifier = Modifier.align(Alignment.Center)) {
-                Button(modifier = Modifier.padding(16.dp),
+                Button(modifier = Modifier
+                    .align(CenterHorizontally)
+                    .padding(16.dp),
                     onClick = {
                         context.startActivity(Intent(context, DetectorActivity::class.java))
                     }) {
                     Text(text = "Camera")
                 }
-                Button(modifier = Modifier.padding(16.dp),
+                Button(modifier = Modifier
+                    .align(CenterHorizontally)
+                    .padding(16.dp),
                     onClick = {
                         context.startActivity(Intent(context, DetectorActivity::class.java))
                     }) {
                     Text(text = "List")
+                }
+
+                Button(modifier = Modifier
+                    .align(CenterHorizontally)
+                    .padding(16.dp),
+                    onClick = {
+                        throw RuntimeException("Intentional crash!")
+                    }) {
+                    Text(text = "Crash")
                 }
             }
         }
